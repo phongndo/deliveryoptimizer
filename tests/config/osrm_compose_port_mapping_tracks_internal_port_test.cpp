af582448c78f4e27efebe784c7c2a76b9b3b9781
phongndo <donguyenphong@icloud.com>
2026-04-17 02:00:16 -0700

    test: assert osrm compose has no host port mapping

--- tests/config/osrm_compose_port_mapping_tracks_internal_port_test.cpp
+++ tests/config/osrm_compose_port_mapping_tracks_internal_port_test.cpp
@@ -13,9 +13,18 @@ TEST(DeployConfigTest, OsrmComposeKeepsOsrmInternalOnly) {
   ASSERT_TRUE(file.is_open()) << "Unable to read " << compose_path;
 
   const std::string content{std::istreambuf_iterator<char>{file}, std::istreambuf_iterator<char>{}};
+  const auto osrm_block_start = content.find("  osrm:\n");
+  ASSERT_NE(osrm_block_start, std::string::npos);
+
+  const auto http_server_block_start = content.find("\n  http-server:\n", osrm_block_start);
+  ASSERT_NE(http_server_block_start, std::string::npos);
+
+  const std::string osrm_block =
+      content.substr(osrm_block_start, http_server_block_start - osrm_block_start);
 
   EXPECT_NE(content.find("OSRM_PORT: ${OSRM_INTERNAL_PORT:-5001}"), std::string::npos);
   EXPECT_EQ(content.find("DELIVERYOPTIMIZER_OSRM_HOST_PORT"), std::string::npos);
   EXPECT_NE(content.find("http://127.0.0.1:${OSRM_INTERNAL_PORT:-5001}/nearest"),
             std::string::npos);
+  EXPECT_EQ(osrm_block.find("\n    ports:\n"), std::string::npos);
 }
